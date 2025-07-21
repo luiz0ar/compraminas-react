38be39e3feded8c3daaa3ada07acd8fe9a98d9e2
luiz0ar <luiz.rangel@alunos.unis.edu.br>
2025-07-21 15:50:48 -0300

    About Page upload image

--- src/components/AboutContent/AboutContent.jsx
+++ src/components/AboutContent/AboutContent.jsx
@@ -3,6 +3,8 @@ import styles from './AboutContent.module.css';
 import aboutMainImage from '../../assets/Conceito-1920x1080-1.png';
 import api from '../../api/api';
 
+const API_URL = api.defaults.baseURL;
+
 function AboutContent() {
   const [pageData, setPageData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -31,16 +33,18 @@ function AboutContent() {
     );
   }
 
-  return (
+  const imageUrl = pageData.image ? `${API_URL}${pageData.image}` : '';
+
+    return (
     <section className={styles.aboutContent}>
       <div className={styles.container}>
         <div className={styles.textColumn}>
           <div className={styles.imageWrapper}>
-            <img src={aboutMainImage} alt="Visão geral do evento Compra Minas" />
+            {imageUrl && <img src={imageUrl} alt="Visão geral do evento Compra Minas" />}
           </div>
           <p className={styles.mainContent}>{pageData.content}</p>
           <h4>Serviço</h4>
-          <p className={styles.serviceInfo}>{pageData.informations}</p>
+           <p className={styles.serviceInfo}>{pageData.informations}</p>
         </div>
       </div>
     </section>
